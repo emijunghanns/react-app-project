fa00e9757449a358d01319a957a12c233d07150f
Emilia Junghanns <emijunghanns@hotmail.com>
2024-11-12 16:03:13 +0100

    added basic components

--- recipe-site/src/App.jsx
+++ recipe-site/src/App.jsx
@@ -1,7 +1,8 @@
 import "./App.css";
+import Homepage from "./Homepage";
 
 function App() {
-  return <div>Hello</div>;
+  return <Homepage />;
 }
 
 export default App;
---
+++ recipe-site/src/Homepage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Sidebar from "./components/Sidebar";
+
+export default function Homepage() {
+  return (
+    <div>
+      <Navbar />
+      <Footer />
+      <Sidebar />
+    </div>
+  );
+}
