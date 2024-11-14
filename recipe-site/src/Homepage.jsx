fcf194fe257ef612876c55978bcf142b32e89c73
Porcari-Karim <porcari.karim1@gmail.com>
2024-11-14 16:28:17 +0100

    showed all recipes

--- recipe-site/src/Homepage.jsx
+++ recipe-site/src/Homepage.jsx
@@ -1,20 +1,16 @@
 import React from "react";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import Sidebar from "./components/Sidebar";
 import RecipeCard from "./components/RecipeCard";
 
 export default function Homepage({ recipeList }) {
-  // console.log(recipeList);
+  console.log(recipeList);
 
   return (
     <div className="homepage">
-      <RecipeCard
-        name={recipeList[5].name}
-        image={recipeList[5].image}
-        calories={recipeList[5].calories}
-        servings={recipeList[5].servings}
-      />
+      {recipeList.map((recipe) => <RecipeCard key={recipe.id} name={recipe.name} 
+      image={recipe.image} 
+      calories={recipe.calories} 
+      servings={recipe.servings}/>
+      )}
     </div>
   );
 }
