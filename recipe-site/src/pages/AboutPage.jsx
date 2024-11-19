431f02e9f7ba290a7c02d819e8b30fe24bd4082d
Emilia Junghanns <emijunghanns@hotmail.com>
2024-11-19 18:06:06 +0100

    about us page

--- recipe-site/src/pages/AboutPage.jsx
+++ recipe-site/src/pages/AboutPage.jsx
@@ -1,10 +1,36 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./AboutPage.css";
 
 export default function AboutPage() {
   return (
-    <Link to="/">
-      <button>Back</button>
-    </Link>
+    <div>
+      <div class="container">
+        <h1>About Us</h1>
+        <div class="bio">
+          <p>
+            We are two passionate web development students working on a
+            delicious project: a website for <span>recipes</span>! Meet the
+            team:
+          </p>
+        </div>
+
+        <div class="team">
+          <div class="person">
+            <h3>Emilia, 31</h3>
+            <p>blah blah</p>
+          </div>
+
+          <div class="person">
+            <h3>Karim, 21</h3>
+            <p>blah blah</p>
+          </div>
+        </div>
+      </div>
+
+      <Link to="/">
+        <button>Back</button>
+      </Link>
+    </div>
   );
 }
