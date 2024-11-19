945a230a22d8558aaad99964a8967da364689745
Emilia Junghanns <emijunghanns@hotmail.com>
2024-11-19 17:56:19 +0100

    details page done

--- recipe-site/src/pages/NotFound.jsx
+++ recipe-site/src/pages/NotFound.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./NotFound.css";
 
 export default function NotFound() {
   return (
     <div>
       <h2>404 Not Found Page </h2>
       <p>Ups... You will not be eating today </p>
-      <Link to="/"> Go Back Home</Link>
+      <Link to="/">
+        <button className="backButton">Try Again!</button>
+      </Link>
     </div>
   );
 }
--- recipe-site/src/pages/RecipeDetails.jsx
+++ recipe-site/src/pages/RecipeDetails.jsx
@@ -1,4 +1,3 @@
-
 import { Link } from "react-router-dom";
 import { useParams } from "react-router-dom";
 
@@ -10,7 +9,13 @@ export default function RecipeDetails({ recipeList }) {
 
   return (
     <div>
-      <h1>{}</h1>
+      <h1>{recipeDetails.name}</h1>
+      <img src={recipeDetails.image}></img>
+      <div>{recipeDetails.calories}</div>
+      <div>{recipeDetails.servings}</div>
+      <p>{recipeDetails.ingredients}</p>
+      <p>{recipeDetails.preparation_steps}</p>
+      <div>{recipeDetails.vegan}</div>
       <Link to="/">
         <button className="backButton">Back</button>
       </Link>
